b7c1eef76af8cf24b975ae7ccdff96fb009d7bf5
willster <marsa027@morris.umn.edu>
2024-05-29 15:29:14 -0500

    added 6 class one hot encoding

--- logistic-regression/preprocessing1.py
+++ logistic-regression/preprocessing1.py
@@ -13,10 +13,69 @@ df.to_csv(f'\\Users\\willg\\OneDrive\\CSCI\\summer-2024-work\\will-grayson-ML\\t
 
 for file in glob.glob('\\Users\\willg\\OneDrive\\CSCI\\summer-2024-work\\will-grayson-ML\\train\\*.csv'):
     file_name = file.split('\\')[-1]
-    
-    df = pd.read_csv(file)
-    df = df.assign(benign=pd.Series([0]*len(df)).values)
-    df = df.assign(attack=pd.Series([1]*len(df)).values)
 
-    # Save the DataFrame to a new csv file
+    if 'Spoofing' in file_name:
+        df = pd.read_csv(file)
+        df = df.assign(spoofing=pd.Series([1]*len(df)).values)
+        df = df.assign(benign=pd.Series([0]*len(df)).values)
+        df = df.assign(MQTT=pd.Series([0]*len(df)).values)
+        df = df.assign(recon=pd.Series([0]*len(df)).values)
+        df = df.assign(DDoS=pd.Series([0]*len(df)).values)
+        df = df.assign(DoS=pd.Series([0]*len(df)).values)
+    
+    if 'Benign' in file_name:
+        df = pd.read_csv(file)
+        df = df.assign(spoofing=pd.Series([0]*len(df)).values)
+        df = df.assign(benign=pd.Series([1]*len(df)).values)
+        df = df.assign(MQTT=pd.Series([0]*len(df)).values)
+        df = df.assign(recon=pd.Series([0]*len(df)).values)
+        df = df.assign(DDoS=pd.Series([0]*len(df)).values)
+        df = df.assign(DoS=pd.Series([0]*len(df)).values)
+    
+    if 'MQTT' in file_name:
+        df = pd.read_csv(file)
+        df = df.assign(spoofing=pd.Series([0]*len(df)).values)
+        df = df.assign(benign=pd.Series([0]*len(df)).values)
+        df = df.assign(MQTT=pd.Series([1]*len(df)).values)
+        df = df.assign(recon=pd.Series([0]*len(df)).values)
+        df = df.assign(DDoS=pd.Series([0]*len(df)).values)
+        df = df.assign(DoS=pd.Series([0]*len(df)).values)
+    
+    if 'Recon' in file_name:
+        df = pd.read_csv(file)
+        df = df.assign(spoofing=pd.Series([0]*len(df)).values)
+        df = df.assign(benign=pd.Series([0]*len(df)).values)
+        df = df.assign(MQTT=pd.Series([0]*len(df)).values)
+        df = df.assign(recon=pd.Series([1]*len(df)).values)
+        df = df.assign(DDoS=pd.Series([0]*len(df)).values)
+        df = df.assign(DoS=pd.Series([0]*len(df)).values)
+    
+    if 'DoS' in file_name:
+        df = pd.read_csv(file)
+        df = df.assign(spoofing=pd.Series([0]*len(df)).values)
+        df = df.assign(benign=pd.Series([0]*len(df)).values)
+        df = df.assign(MQTT=pd.Series([0]*len(df)).values)
+        df = df.assign(recon=pd.Series([0]*len(df)).values)
+        df = df.assign(DDoS=pd.Series([0]*len(df)).values)
+        df = df.assign(DoS=pd.Series([1]*len(df)).values)
+
+    if 'DDoS' in file_name:
+        df = pd.read_csv(file)
+        df = df.assign(spoofing=pd.Series([0]*len(df)).values)
+        df = df.assign(benign=pd.Series([0]*len(df)).values)
+        df = df.assign(MQTT=pd.Series([0]*len(df)).values)
+        df = df.assign(recon=pd.Series([0]*len(df)).values)
+        df = df.assign(DDoS=pd.Series([1]*len(df)).values)
+        df = df.assign(DoS=pd.Series([0]*len(df)).values)
+    
     df.to_csv(f'\\Users\\willg\\OneDrive\\CSCI\\summer-2024-work\\will-grayson-ML\\six-class-labeled-data\\train\\{file_name}', index=False)
+
+    
+'''
+df = pd.read_csv(file)
+df = df.assign(benign=pd.Series([0]*len(df)).values)
+df = df.assign(attack=pd.Series([1]*len(df)).values)
+
+# Save the DataFrame to a new csv file
+df.to_csv(f'\\Users\\willg\\OneDrive\\CSCI\\summer-2024-work\\will-grayson-ML\\six-class-labeled-data\\train\\{file_name}', index=False)
+'''
